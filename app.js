396a0c51203e2146b5ca63b18ac03359e4474920
Nagaphaneendra <Nagaphaneendra6240@outlook.com>
2023-03-02 17:46:49 +0000

    API 3 modified

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.put("/players/:playerId/", async (request, response) => {
      UPDATE
      player_details
      SET
-     player_name = ${playerName}
+     player_name = '${playerName}'
      WHERE player_id = ${playerId};`;
   await db.run(playerQuery);
   response.send("Player Details Updated");
